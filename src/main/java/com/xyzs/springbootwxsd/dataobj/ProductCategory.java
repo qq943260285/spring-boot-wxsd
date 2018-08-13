5aaff71b00a75bb26f45b826bc0c88a2b2102a9c
xiaoyu <943260285@qq.com>
2018-08-14 07:58:06 +0800

    +

--- src/main/java/com/xyzs/springbootwxsd/dataobj/ProductCategory.java
+++ src/main/java/com/xyzs/springbootwxsd/dataobj/ProductCategory.java
@@ -3,7 +3,6 @@ package com.xyzs.springbootwxsd.dataobj;
 import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/com/xyzs/springbootwxsd/dataobj/ProductInfo.java
+++ src/main/java/com/xyzs/springbootwxsd/dataobj/ProductInfo.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
 import java.math.BigDecimal;
 import java.util.Date;
 
